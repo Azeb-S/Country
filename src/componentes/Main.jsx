7d06a7776ccb1e12114dee70fbf15b844f645d0d
Azeb Tesfay <azbseb@gmail.com>
2026-05-15 17:13:30 -0700

    added style and search bar and search button

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -3,40 +3,81 @@ import CountryItem from "./CountryItem"
 
 export default function Main() {
     const [countries, setCountries] = useState([])
-    const [loading, setLoading ] = useState(true)
+    const [loading, setLoading] = useState(true)
     const [error, setError] = useState("")
+    const [searchTerm, setSearchTerm] = useState("")
 
-    useEffect (() => {
+
+    useEffect(() => {
 
         fetch("https://restcountries.com/v3.1/all?fields=name,capital,currencies")
-        .then(res => {
-            if(!res.ok) {
-                throw Error ("Failed to fetch countries")
-            }
-            return res.json()
-        })
-        .then(data => {
-            setCountries(data)
-            setLoading(false)
-        })
-        .catch(err => {
-            setError(err.message)
-            setLoading(false)
-        })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Failed to fetch countries")
+                }
+                return res.json()
+            })
+            .then(data => {
+                setCountries(data)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
     }, [])
 
-    if(loading) {
-        return <h2>Loading countries</h2>
+    function searchCountry(event) {
+        event.preventDefault()
+
+        setLoading(true)
+        setError("")
+
+        fetch(
+            `https://restcountries.com/v3.1/name/${searchTerm}?fields=name,capital,currencies`
+        )
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Country not found")
+                }
+                return res.json()
+            })
+            .then(data => {
+                setCountries(data)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+
+
     }
 
-    if(error) {
-        return <h2>Error: {error}</h2>
+    if (loading) {
+        return <main><h2>Loading countries</h2></main>
+    }
+
+    if (error) {
+        return <main><h2>Error: {error}</h2></main>
     }
 
     return (
         <main>
-          {countries.slice(0, 10).map(country => (
-                <CountryItem 
+            <form onSubmit={searchCountry}>
+
+                <input
+                    type="text"
+                    placeholder="Search country"
+                    value={searchTerm}
+                    onChange={(event) =>
+                        setSearchTerm(event.target.value)
+                    } />
+                <button>Search</button>
+            </form>
+
+            {countries.slice(0, 10).map(country => (
+                <CountryItem
                     key={country.name.common}
                     country={country}
                 />
